615c9be41d6b79bc5875f68b5803ed3da3682c68
DevvObiero <obieropauldev@gmail.com>
2024-10-22 12:32:26 +0300

    i take it back!!!!I'm awesome!!!!!

--- script.js
+++ script.js
@@ -6,24 +6,5 @@ window.addEventListener('scroll', function() {
   const leftDiv = document.querySelector('.left');
   const rightDiv = document.querySelector('.right');
   
-  // Get the scroll position of the window
-  const scrollY = window.scrollY;
-  
-  // Define scroll threshold values
-  const leftScrollThreshold = 200;  // When the left section becomes fixed
-  const rightScrollThreshold = 800; // When the right section becomes fixed
-  
-  // Make left static/fixed based on scroll
-  if (scrollY >= leftScrollThreshold && scrollY < rightScrollThreshold) {
-    leftDiv.classList.add('fixed');
-  } else {
-    leftDiv.classList.remove('fixed');
-  }
 
-  // Make right static/fixed based on scroll
-  if (scrollY >= rightScrollThreshold) {
-    rightDiv.classList.add('fixed');
-  } else {
-    rightDiv.classList.remove('fixed');
-  }
 })
